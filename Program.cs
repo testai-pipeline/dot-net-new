adf95337ba31135838d44175e7ff6fa3e402c98b
himanshi.gupta@562 <himanshi.gupta@ratnaglobaltech.com>
2026-06-26 11:32:52 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ namespace HelloWorld
 
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
-
             Console.WriteLine("Hello, " + name + "!");
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
